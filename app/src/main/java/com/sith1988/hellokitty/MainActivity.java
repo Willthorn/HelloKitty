8e8db423bc54df310dbf4f89b9ab6b542bf577f6
Vil Gilyzetdinov <sith1988@gmail.com>
2016-03-17 12:36:03 +0500

    Добавил третий выбор, прикрутил показ изображений по нажатию на радиокнопку.

--- app/src/main/java/com/sith1988/hellokitty/MainActivity.java
+++ app/src/main/java/com/sith1988/hellokitty/MainActivity.java
@@ -4,27 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mHelloTextView;
+    private ImageView mImageView;
     private EditText mNameTextEdit;
+    private RadioButton mButton1;
+    private RadioButton mButton2;
+    private RadioButton mButton3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHelloTextView = (TextView)findViewById(R.id.textView);
-        mNameTextEdit = (EditText)findViewById(R.id.editText);
+        mImageView = (ImageView)findViewById(R.id.imageView);
+        mButton1 = (RadioButton)findViewById(R.id.radioButton);
+        mButton2 = (RadioButton)findViewById(R.id.radioButton2);
+        mButton3 = (RadioButton)findViewById(R.id.radioButton3);
     }
 
     public void onClick(View view) {
 
-        if(mNameTextEdit.getText().length()!=0) {
-            mHelloTextView.setText("Hello "+mNameTextEdit.getText()+"!");
-        } else {
-            mHelloTextView.setText("Hello Kitty!");
+        if(mButton1.isChecked())
+            mImageView.setImageResource(R.drawable.dog);
+        if(mButton2.isChecked())
+            mImageView.setImageResource(R.drawable.kitten);
+        if(mButton3.isChecked())
+            mImageView.setImageResource(R.drawable.bird);
+
         }
 
-    }
 }
+
